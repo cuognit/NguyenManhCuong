bdffa550bb851ab2573423bcf8aaf8bda91c2469
cuognit <cuongct18.jr@gmail.com>
2024-11-10 14:02:30 +0700

    bai1

--- BaiTapOopJava/src/Chuong2/Bai1/test.java
+++ BaiTapOopJava/src/Chuong2/Bai1/test.java
@@ -2,6 +2,19 @@ package Chuong2.Bai1;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("âdad");
+        Person ps1 = new Person();
+        ps1.setName("Cuong");
+        ps1.setDob("12/2/2020");
+        ps1.setPob("Ha Noi");
+        ps1.setGender('M');
+        ps1.setPhone("0987654321");
+        ps1.setEmail("abcxyz@gmail.com");
+        System.out.println(ps1.getName());
+        System.out.println(ps1.getDob());
+        System.out.println(ps1.getPob());
+        System.out.println(ps1.getGender());
+        System.out.println(ps1.getEmail());
+        System.out.println(ps1.getPhone());
+        System.out.println(ps1.toString());
     }
 }
